2fe7d6192adc86a616518b024b7d7df1722b9e00
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-03-25 23:27:40 +0100

    Formating and some shit

--- bot.py
+++ bot.py
@@ -5,31 +5,31 @@ import datetime
 TOKEN = 'NjkyNDYyMDMzMzk1Nzc3NTY2.Xnu8sQ.nX0UH4jhZrO-nNT9p6gRuCy-I7M'
 bot = commands.Bot(command_prefix='$')
 
+
 @bot.command()
 async def ping(ctx):
-     await ctx.send("Pong!!")
+    await ctx.send("Pong!!")
+
 
 @bot.command()
 async def wiesel(ctx):
+    file = discord.File("wiesel.jpg")
+    await ctx.send("", file=file)
 
-    with open('wiesel.jpg', 'r') as f:
-        file = discord.File("wiesel.jpg")
-        await ctx.send("", file=file)
 
 @bot.command()
-async def quote(ctx, quote, time=None):
-
+async def quote(ctx, _quote, time=None):
     if time is None:
         time = datetime.datetime.now()
         time = time.strftime("%Y-%m-%d %H:%M:%S")
     with open('quotes.txt', 'a') as q:
-        q.write(quote + '/' + time + '\n')
+        q.write(_quote + '/' + time + '\n')
 
     await ctx.send("Quote saved")
 
+
 @bot.command()
 async def clear(ctx, amount=100):
-
     for role in ctx.author.roles:
 
         if role == "Moderator" or discord.ext.commands.has_permissions(administrator=True):
@@ -37,9 +37,9 @@ async def clear(ctx, amount=100):
             await ctx.send("Channel cleared")
             return
 
+
 @bot.command()
 async def show_quotes(ctx):
-
     with open('quotes.txt') as f:
         lines = f.readlines()
         line_string = "".join(lines)
@@ -53,22 +53,22 @@ async def on_ready():
 
 def check_files():
     try:
-        with open("quotes.txt", 'r') as q:
+        with open("quotes.txt", 'r') as _:
             pass
 
     except FileNotFoundError:
-        with open('quotes.txt', 'w+') as q:
+        with open('quotes.txt', 'w+') as _:
             pass
 
     try:
-        with open("roles.json", 'r') as r:
+        with open("roles.json", 'r') as _:
             pass
 
     except FileNotFoundError:
-        with open('roles.josn', 'w+') as r:
+        with open('roles.josn', 'w+') as _:
             pass
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     check_files()
     bot.run(TOKEN)
